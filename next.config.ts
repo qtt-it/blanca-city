a689528a42c545d0411a1b29982aa2f786d41c5d
TrungThanh12081997 <quantrungthanh73@gmail.com>
2025-06-04 20:23:23 +0700

    [cd-blancacity]

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
-
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
